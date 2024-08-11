3f94f53383db00ebe6b1795cad34fcfa213936f7
Mike Stavrakopoulos <19961621+MikeStavr@users.noreply.github.com>
2024-08-11 13:34:18 +0300

    change images locations

--- src/projects.ts
+++ src/projects.ts
@@ -4,21 +4,21 @@ export const projects = [
   new Project(
     "Restaurant Management System",
     "A restaurant management system for managing orders and reservations written in PHP and MySQL.",
-    "https://github.com/MikeStavr/portfolio_page/blob/main/public/images/php_mysql_proj.png?raw=true",
+    "./assets/images/php_mysql_proj.png",
     "https://github.com/MikeStavr/Internet-Technologies-Project",
     "#"
   ),
   new Project(
     "Menu management App",
     "A menu management app for managing restaurant menus written in React and MySQL.",
-    "https://github.com/MikeStavr/portfolio_page/blob/main/public/images/restaurant_react_menu.png?raw=true",
+    "./assets/images/restaurant_react_menu.png",
     "https://github.com/MikeStavr/Restaurant-React",
     "#"
   ),
   new Project(
     "Portfolio page",
     "This portfolio page, single-page application written in React.",
-    "https://github.com/MikeStavr/portfolio_page/blob/main/public/images/portfolio_page_preview.png?raw=true",
+    "./assets/images/portfolio_page_preview.png",
     "https://github.com/MikeStavr/portfolio_page",
     "#"
   ),
